bf68f7b36578ad4f13ef090d1a637224572bc654
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-03-28 18:01:01 -0300

    add new simple presentation

---
+++ src/main/java/io/github/gleidsonmt/presentation/SimplePresentation.java
@@ -0,0 +1,10 @@
+package io.github.gleidsonmt.presentation;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  28/03/2025
+ */
+public class SimplePresentation extends Presentation<SimplePresentation> {
+
+
+}
