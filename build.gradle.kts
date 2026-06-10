6373d04236ab92f8685ed6b80dd3dbfa0531a3ee
Gleidson Neves <gleidisonmt@gmail.com>
2026-06-09 22:54:39 -0300

    preparing to merge with alpha

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(23)
     }
+    modularity.inferModulePath = true
     // The line resolves the problem with the Java Language Server in VS Code, but it is unnecessary for Gradle run.
     // Without this line, Gradle run works, but the Java Language Server in VS Code reports “module not found” for module jars that are on the Gradle module path.
     // modularity.inferModulePath = true
